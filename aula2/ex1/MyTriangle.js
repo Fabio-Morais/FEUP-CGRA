f1ab0c37ea29b8dd8b3597954ed6199dc13a8215
fábio <fabiouds@hotmail.com>
2020-03-02 22:15:28 +0000

    update

---
+++ aula2/ex1/MyTriangle.js
@@ -0,0 +1,30 @@
+/**
+ * MyDiamond
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyTriangle extends CGFobject {
+	constructor(scene) {
+		super(scene);
+		this.initBuffers();
+	}
+	initBuffers() {
+		this.vertices = [
+			-1, 0, 0,	//0		XA,YA,ZA
+			1, 0, 0,	//1		XB,YB,ZB
+			0, 1, 0,	//2
+		];
+
+		//Counter-clockwise reference of vertices
+		this.indices = [
+			0, 1, 2,	// indices dos vertices acima
+		];
+/*anti horario-> parte da frente
+sentido horario -> parte de trás  */ 
+		//The defined indices (and corresponding vertices)
+		//will be read in groups of three to draw triangles
+		this.primitiveType = this.scene.gl.TRIANGLES;
+
+		this.initGLBuffers();
+	}
+}
